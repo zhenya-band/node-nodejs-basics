acf8b31fd7cc475439b9d0cd3e9ff8878e5d821e
Yauheni Bandziukevich <yauheni.bandziukevich@innowise-group.com>
2022-11-06 16:59:25 +0300

    feat: add copy, create, rename functions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,22 @@
+import fs from 'fs'
+import { cp } from 'fs/promises'
+import path from 'path';
+
+const __dirname = path.resolve();
+
 export const copy = async () => {
-    // Write your code here 
+    const srcPath = path.join(__dirname, 'src/fs/files')
+    const destinationPath = path.join(__dirname, 'src/fs/files_copy')
+
+    if (fs.existsSync(destinationPath)) {
+        throw Error('FS operation failed');
+    }
+
+    try {
+        await cp(srcPath, destinationPath, { recursive: true })
+    } catch (error) {
+        throw Error('FS operation failed');
+    }
 };
+
+copy();
