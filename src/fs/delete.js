d8880d94010e37f55b05e6f40aedf002bce0267d
Yauheni Bandziukevich <yauheni.bandziukevich@innowise-group.com>
2022-11-23 21:28:50 +0300

    feat: implement delete & list functions

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { unlink } from 'fs/promises';
+import path from 'path';
+
+const __dirname = path.resolve();
+
 const remove = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, 'src/fs/files/fileToRemove.txt');
+    try {
+        await unlink(pathToFile)
+    } catch (error) {
+        throw Error('FS operation failed');
+    }
 };
 
 await remove();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { readdir } from 'fs/promises';
+import path from 'path';
+
+const __dirname = path.resolve();
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = path.join(__dirname, 'src/fs/files');
+
+    try {
+        const files = await readdir(folderPath);
+        console.log(files);
+    } catch (error) {
+        throw Error('FS operation failed');
+    }
 };
 
 await list();
