adb4550b834fdec6cd82a37f5aee35cae7d5e7b8
Yauheni Bandziukevich <yauheni.bandziukevich@innowise-group.com>
2022-11-26 12:55:28 +0300

    feat: implement cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const result = args
+        .map((arg, index) => {
+            if (arg.startsWith('--')) {
+                const key = arg.slice(2);
+                return `${key} is ${args[index + 1]}`
+            }
+        })
+        .filter(Boolean)
+        .join(', ');
+
+    console.log(result);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envs = process.env;
+    const result = Object.entries(envs)
+        .filter(([key]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; ');
+
+    console.log(result);
 };
 
 parseEnv();
