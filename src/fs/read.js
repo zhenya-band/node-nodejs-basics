46ca36e35d543370371aad7dbf88ad8be96f8ead
Yauheni Bandziukevich <yauheni.bandziukevich@innowise-group.com>
2022-11-26 12:08:04 +0300

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from 'fs/promises';
+
+import path from 'path';
+
+const __dirname = path.resolve();
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, 'src/fs/files/fileToRead.txt');
+    try {
+        const content = await readFile(pathToFile, { encoding: 'utf8' });
+        console.log(content);
+    } catch (error) {
+        throw Error('FS operation failed');
+    }
 };
 
 await read();
