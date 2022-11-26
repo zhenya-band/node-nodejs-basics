c26855fe5354d0460a2524dfb274e4b254b0c8b1
Yauheni Bandziukevich <yauheni.bandziukevich@innowise-group.com>
2022-11-26 13:16:23 +0300

    feat: implement calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from 'node:crypto'
+import { readFile } from 'node:fs/promises';
+import path from 'path';
+
+const __dirname = path.resolve();
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const pathToFile = path.join(__dirname, 'src/hash/files/fileToCalculateHashFor.txt');
+        const content = await readFile(pathToFile, { encoding: 'utf8' })
+        const hash = createHash('sha256').update(content).digest('hex');
+        console.log(hash);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await calculateHash();
