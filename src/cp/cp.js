4f42cf84b7347a114ee5053e8a10bfafb8f99a2c
Yauheni Bandziukevich <yauheni.bandziukevich@innowise-group.com>
2022-12-04 21:46:35 +0300

    feat: implement child_process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import { fork } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename =  fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToScript = path.join(__dirname, '/files/script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    fork(pathToScript, args);
 };
 
-spawnChildProcess();
+spawnChildProcess(["test1", "test2"]);
